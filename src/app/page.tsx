267f5d4a2ce93ed2832c7b06fda2a96c6997bd56
WoraPhet <Worathutj4@gmail.com>
2024-02-08 09:25:58 +0700

    fix config option
    fix config option, add run button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,15 @@ const onChange = (value: string, ev: any[]) => {
   console.log(value);
 };
 
+const handleOnClick = () => {
+  console.log("Button clicked");
+};
+
 const options = {
   autoIndent: "full",
   contextmenu: true,
   fontFamily: "monospace",
-  fontSize: 13,
+  fontSize: 16,
   lineHeight: 24,
   hideCursorInOverviewRuler: true,
   matchBrackets: "always",
@@ -24,19 +28,36 @@ const options = {
   selectOnLineNumbers: true,
   roundedSelection: false,
   readOnly: false,
-  cursorStyle: "underline-thin",
+  cursorStyle: "line",
   automaticLayout: true,
+
+  LineNumber: "on",
+  padding: {
+    top: 5,
+  },
 };
 
 export default function Home() {
   return (
-    <Editor
-      height="100vh"
-      defaultLanguage="javascript"
-      defaultValue="// some comment"
-      onChange={onChange}
-      theme="vs-dark"
-      options={options}
-    />
+    <>
+      <div className="flex-row">
+        <div className="grid justify-items-end m-1 mr-5 ">
+          <button
+            onClick={handleOnClick}
+            className="px-5 py-2 bg-green-600 rounded-md"
+          >
+            Run▸
+          </button>
+        </div>
+        <Editor
+          height="60vh"
+          defaultLanguage="c"
+          defaultValue="// some comment"
+          onChange={onChange}
+          theme="vs-dark"
+          options={options}
+        />
+      </div>
+    </>
   );
 }
