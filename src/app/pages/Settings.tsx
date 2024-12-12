dfed16f1ed0e172dbdc54e372bace7ea0c8bee8e
patiphan01 <86480648+paan1@users.noreply.github.com>
2024-12-12 21:45:13 +0700

    update login and setting

--- src/app/pages/Settings.tsx
+++ src/app/pages/Settings.tsx
@@ -17,6 +17,7 @@ export default function Setting() {
     newPassword: "**********************",
     confirmPassword: "**********************",
     gender: "",
+    role: "",
     profilePicture: Profileuser,
   });
 
@@ -55,6 +56,7 @@ export default function Setting() {
           newPassword: "**********************",
           confirmPassword: "**********************",
           gender: response.data.gender,
+          role: response.data.role,
           profilePicture: response.data.picture || Profileuser, // Use default if no picture
         });
       } catch (error) {
@@ -72,50 +74,46 @@ export default function Setting() {
   };
 
   return (
-    <div className="flex flex-col md:flex-row gap-10 p-8 min-h-screen bg-[#0F172A]">
-      <div className="w-full md:w-1/3 flex flex-col">
-        <h2 className="text-3xl text-white mb-6 font-bold">โปรไฟล์</h2>
-        <div className="rounded-lg p-6 flex flex-col bg-[#16233A] shadow-lg border border-[#1E293B] h-10/12 space-y-8">
+    <div className="gap-10 p-8 h-full w-full">
+      <div>
+        <h2 className="text-3xl text-white mb-8 font-bold">โปรไฟล์</h2>
+        <div className="rounded-lg p-8 flex justify-center items-start shadow-lg space-x-8 w-full mx-auto">
           {loading ? (
             <SkeletonLoader />
           ) : (
             <>
-              <div className="flex items-center mb-6">
-                <div className="w-36 h-36 rounded-full border-4 border-[#3b4f61] shadow-lg overflow-hidden ">
-                  <Image
-                    src={
-                      profileData.profilePicture && (profileData.profilePicture.startsWith("http") || profileData.profilePicture.startsWith("/"))
-                        ? profileData.profilePicture
-                        : Profileuser
-                    }
-                    className="w-full h-full object-cover object-center"
-                    width={144}
-                    height={144}
-                    alt="Profile Picture"
-                  />
+              <div className="flex flex-col items-center justify-center">
+                <div className="w-48 h-48 rounded-full border-4 border-[#3b4f61] shadow-lg overflow-hidden mb-6">
+                  <Image src={profileData.profilePicture} alt="Profile" width={192} height={192} className="object-cover" />
                 </div>
-
-
-                <div className="ml-4">
-                  <h1 className="text-3xl font-bold mb-2 text-white">{profileData.username}</h1>
-                  <p className="text-gray-400">{profileData.email}</p>
-                  <button
-                    onClick={handleEditClick}
-                    className={`text-white mt-3 py-2 px-4 rounded-md font-semibold text-lg transition-colors duration-300 shadow-md ${isEditing ? "bg-[#0099FF] hover:bg-[#007bb5]" : "bg-[#475766] hover:bg-[#1f3a47]"
-                      }`}
-                  >
-                    {isEditing ? "เปลี่ยนโปรไฟล์" : "แก้ไขโปรไฟล์"}
-                  </button>
-                </div>
+                <button
+                  onClick={handleEditClick}
+                  className={`text-white py-2 px-6 rounded-md font-semibold text-lg transition-colors duration-300 shadow-md ${isEditing ? "bg-[#0099FF] hover:bg-[#007bb5]" : "bg-[#475766] hover:bg-[#1f3a47]"}`}
+                >
+                  {isEditing ? "เปลี่ยนโปรไฟล์" : "แก้ไขโปรไฟล์"}
+                </button>
               </div>
 
-              <div className="flex flex-col text-white space-y-8">
-                <ProfileField label="ชื่อผู้ใช้งาน" name="username" value={profileData.username} isEditing={isEditing} onChange={handleChange} />
-                <ProfileField label="ชื่อจริง" name="firstName" value={profileData.firstName} isEditing={isEditing} onChange={handleChange} />
-                <ProfileField label="นามสกุล" name="lastName" value={profileData.lastName} isEditing={isEditing} onChange={handleChange} />
-                <div className="flex items-center space-x-4">
+              <div className="flex flex-col text-white space-y-4 w-1/2 pt-5">
+                <h1 className="text-4xl font-bold">{profileData.username}</h1>
+                <p className="text-lg text-gray-400">{profileData.email}</p>
+                <p className="text-lg pb-5">{profileData.role} สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง</p>
+
+                <div className="flex flex-col space-y-6 pt-5">
+                  <div className="flex justify-between space-x-6 ">
+                    <ProfileField label="ชื่อผู้ใช้งาน" name="username" value={profileData.username} isEditing={isEditing} onChange={handleChange} />
+                    <ProfileField label="อีเมล" name="email" value={profileData.email} isEditing={isEditing} onChange={handleChange} />
+                  </div>
+
+                  <div className="flex justify-between space-x-6 ">
+                    <ProfileField label="ชื่อจริง" name="firstName" value={profileData.firstName} isEditing={isEditing} onChange={handleChange} />
+                    <ProfileField label="นามสกุล" name="lastName" value={profileData.lastName} isEditing={isEditing} onChange={handleChange} />
+                  </div>
+                </div>
+
+                <div className="flex items-center space-x-6 mt-6">
                   <label className="text-lg font-medium text-gray-300">เพศ:</label>
-                  <div className="flex items-center space-x-4">
+                  <div className="flex items-center space-x-6">
                     <GenderRadio value="male" checked={profileData.gender === "MALE"} onChange={handleChange} />
                     <GenderRadio value="female" checked={profileData.gender === "FEMALE"} onChange={handleChange} />
                     <GenderRadio value="other" checked={profileData.gender === "other"} onChange={handleChange} />
@@ -125,54 +123,29 @@ export default function Setting() {
             </>
           )}
         </div>
+        <div className="absolute flex item-center right-5  bottom-0 space-x-4 mt-6">
+          <button
+            onClick={handleSave} // หรือฟังก์ชันอื่นๆ ตามต้องการ
+            className="text-white py-2 px-6 rounded-md font-semibold text-lg transition-colors duration-300 shadow-md bg-[#475766] hover:bg-[#1f3a47]"
+          >
+            แก้ไขข้อมูล
+          </button>
+          <button
+            onClick={handleSave} // หรือฟังก์ชันที่เกี่ยวข้องกับการเปลี่ยนรหัสผ่าน
+            className="text-white py-2 px-6 rounded-md font-semibold text-lg transition-colors duration-300 shadow-md bg-[#475766] hover:bg-[#1f3a47]"
+          >
+            เปลี่ยนรหัสผ่าน
+          </button>
+        </div>
       </div>
-
-      <div className="flex flex-col w-full md:w-2/3 gap-6">
-        {loading ? (
-          <>
-            <p className="text-3xl text-white font-bold">ช่องทางติดต่อ</p>
-            <SkeletonField />
-            <p className="text-3xl text-white font-bold">รีเซ็ตรหัสผ่าน</p>
-            <SkeletonField />
-          </>
-        ) : (
-          <>
-            <ContactSection profileData={profileData} isEditing={isEditing} handleChange={handleChange} />
-            <ResetPasswordSection profileData={profileData} isEditing={isEditing} handleChange={handleChange} />
-            {isEditing && (
-              <div className="flex justify-end gap-4 mt-6">
-                <button
-                  onClick={handleCancel}
-                  className="py-2 px-4 bg-[#F5F5F5] text-black rounded-md hover:bg-[#2c4a5b] transition-colors duration-300"
-                >
-                  ยกเลิก
-                </button>
-                <button
-                  onClick={handleSave}
-                  className="py-2 px-4 bg-[#0099FF] text-white rounded-md hover:bg-[#007bb5] transition-colors duration-300"
-                >
-                  บันทึก
-                </button>
-              </div>
-            )}
-          </>
-        )}
-      </div>
-
-      <input
-        id="fileInput"
-        type="file"
-        accept="image/*"
-        onChange={handleFileChange}
-        className="hidden"
-      />
     </div>
+
   );
 }
 
 const ProfileField = ({ label, name, value, isEditing, onChange }: any) => (
-  <div className="flex items-center">
-    <span className="font-medium w-32 text-gray-300">{label}:</span>
+  <div className="flex flex-col items-start space-y-1 w-full">
+    <span className="font-medium text-gray-300">{label}:</span>
     {isEditing ? (
       <input
         type="text"
@@ -182,7 +155,7 @@ const ProfileField = ({ label, name, value, isEditing, onChange }: any) => (
         className="pl-5 py-3 w-full text-white bg-[#2A3A50] rounded-md border border-gray-300 shadow-inner"
       />
     ) : (
-      <div className="pl-3">{value}</div>
+      <div className="pl-3 text-white border border-[#2A3A50] rounded-md py-3 w-full">{value}</div>
     )}
   </div>
 );
@@ -197,100 +170,19 @@ const GenderRadio = ({ value, checked, onChange }: any) => (
       onChange={onChange}
       className="hidden peer"
     />
-    <div className={`w-5 h-5 flex items-center justify-center border-2 rounded-full ${checked ? "bg-[#15A7D5] border-transparent" : "border-[#15A7D5]"}`}>
-      {checked && <div className="w-2 h-2 bg-[#2A3A50] rounded-full"></div>}
+    <div className={`w-6 h-6 flex items-center justify-center border-2 rounded-full ${checked ? "bg-[#15A7D5] border-transparent" : "border-[#15A7D5]"}`}>
+      {checked && <div className="w-3 h-3 bg-[#2A3A50] rounded-full"></div>}
     </div>
     <span className="text-white">{value === "male" ? "ชาย" : value === "female" ? "หญิง" : "อื่นๆ"}</span>
   </label>
 );
 
-const ContactSection = ({ profileData, isEditing, handleChange }: any) => (
-  <>
-    <h2 className="text-3xl text-white font-bold">ช่องทางติดต่อ</h2>
-    <div className="bg-[#16233A] p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 border border-[#1E293B] space-y-6">
-      <div className="mb-6">
-        <h2 className="text-2xl font-bold text-white mb-2">โรงเรียน/สถาบัน</h2>
-        {isEditing ? (
-          <input
-            type="text"
-            name="school"
-            value={profileData.school}
-            onChange={handleChange}
-            className="w-full px-4 py-2 bg-[#2A3A50] rounded-md text-white"
-          />
-        ) : (
-          <div className="text-white">{profileData.school}</div>
-        )}
-      </div>
-      <div>
-        <h2 className="text-2xl font-bold text-white mb-2">อีเมล</h2>
-        {isEditing ? (
-          <input
-            type="email"
-            name="email"
-            value={profileData.email}
-            onChange={handleChange}
-            className="w-full px-4 py-2 bg-[#2A3A50] rounded-md text-white"
-          />
-        ) : (
-          <div className="text-white">{profileData.email}</div>
-        )}
-      </div>
-    </div>
-  </>
-);
-
-const ResetPasswordSection = ({ profileData, isEditing, handleChange }: any) => (
-  <>
-    <h2 className="text-3xl text-white font-bold mt-6">รีเซ็ตรหัสผ่าน</h2>
-    <div className="bg-[#16233A] p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 border border-[#1E293B] space-y-6">
-      <div className="mb-4">
-        <h2 className="text-2xl font-bold text-white mb-2">รหัสผ่านใหม่</h2>
-        {isEditing ? (
-          <input
-            type="password"
-            name="newPassword"
-            value={profileData.newPassword}
-            onChange={handleChange}
-            className="w-full px-4 py-2 bg-[#2A3A50] rounded-md text-white"
-          />
-        ) : (
-          <div className="text-white">**********************</div>
-        )}
-      </div>
-      <div>
-        <h2 className="text-2xl font-bold text-white mb-2">ยืนยันรหัสผ่านใหม่</h2>
-        {isEditing ? (
-          <input
-            type="password"
-            name="confirmPassword"
-            value={profileData.confirmPassword}
-            onChange={handleChange}
-            className="w-full px-4 py-2 bg-[#2A3A50] rounded-md text-white"
-          />
-        ) : (
-          <div className="text-white">**********************</div>
-        )}
-      </div>
-    </div>
-  </>
-);
-
 const SkeletonLoader = () => (
-  <div className="animate-pulse flex flex-col space-y-4">
-    <div className="h-36 bg-gray-600 rounded-full"></div>
-    <div className="h-8 bg-gray-600 rounded w-2/3"></div>
-    <div className="h-6 bg-gray-600 rounded w-1/3"></div>
+  <div className="animate-pulse flex flex-col space-y-6">
+    <div className="h-48 bg-gray-600 rounded-full"></div>
+    <div className="h-10 bg-gray-600 rounded w-2/3"></div>
     <div className="h-8 bg-gray-600 rounded w-1/2"></div>
-    <div className="h-8 bg-gray-600 rounded w-1/4"></div>
-  </div>
-);
-
-const SkeletonField = () => (
-  <div className="animate-pulse flex flex-col space-y-4">
-    <div className="h-8 bg-gray-600 rounded w-1/3"></div>
-    <div className="h-6 bg-gray-600 rounded w-2/3"></div>
-    <div className="h-8 bg-gray-600 rounded w-1/2"></div>
-    <div className="h-8 bg-gray-600 rounded w-1/4"></div>
+    <div className="h-10 bg-gray-600 rounded w-1/4"></div>
+    <div className="h-10 bg-gray-600 rounded w-1/3"></div>
   </div>
 );
