03aafd8e283838abd37306bf7e467373139bf369
Moji <ppawnn2545@gmail.com>
2025-02-03 15:06:02 +0700

    Update page

--- src/app/admin/school/page.tsx
+++ src/app/admin/school/page.tsx
@@ -28,38 +28,98 @@ export default function school() {
             </div>
           </div>
 
-          <div className="flex flex-col items-start gap-6 self-stretch">
-            <div className="flex items-center gap-2.5 p-2 px-6 rounded-md self-stretch bg-[#304972] bg-opacity-30">
-              <span className="flex w-[540px] h-[45px] items-center gap-5 text-[18px]	text-neutral-50">ชื่อ</span>
-              <span className="flex w-[658px] h-[45px] items-center gap-5 text-[18px]	text-neutral-50">ที่ตั้ง</span>
-              <span className="flex w-[170px] h-[45px] justify-center items-center gap-5 text-[18px]	text-neutral-50">ผู้สอน</span>
-              <span className="flex w-[170px] h-[45px] justify-center items-center gap-5 text-[18px]	text-neutral-50">ผู้เรียน</span>
-              <span className="flex w-[36px] h-[45px] items-center gap-5text-[18px]	text-neutral-50"></span>
-            </div>
+          <div className="w-full overflow-x-auto">
+            <table className="table-auto w-full border-collapse text-neutral-50">
+              <thead>
+                <tr className="bg-[#304972] bg-opacity-30 text-[18px]">
+                  <th className="px-6 py-3 rounded-l-md !font-normal truncate">ชื่อ</th>
+                  <th className="px-6 py-3 !font-normal truncate">ที่ตั้ง</th>
+                  <th className="px-6 py-3 !font-normal truncate">ผู้สอน</th>
+                  <th className="px-6 py-3 !font-normal truncate">ผู้เรียน</th>
+                  <th className="px-6 py-3 rounded-r-md !font-normal"></th>
+                </tr>
+              </thead>
+              <tbody>
+                {/* data in Table */}
+                <tr className="justify-center">
+                  <td className="px-6 py-4">
+                    <div className="flex items-center gap-5 truncate">
+                      <img
+                        src="https://www.eng.kmitl.ac.th/wp-content/uploads/2024/06/About-4-B.png"
+                        alt="icon"
+                        className="w-[60px] h-[60px] min-w-[60px] min-h-[60px] flex-shrink-0"
+                      />
+                      <span className="truncate">สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง</span>
+                    </div>
+                  </td>
 
-            {/* card data school */}
-            <div className="flex flex-col items-start gap-6 self-stretch">
-              <div className="flex items-center gap-2.5 p-2 px-6 rounded-md self-stretch">
-                <span className="flex w-[540px] h-[60px] items-center gap-5 text-[18px]	text-neutral-50">
-                  <img src="https://www.eng.kmitl.ac.th/wp-content/uploads/2024/06/About-4-B.png" alt="icon" className="w-[60px] h-[60px]" />
-                  สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง</span>
-                <span className="flex w-[658px] h-[60px] items-center gap-5 text-[14px]	text-neutral-50">1 ซอยฉลองกรุง 1 เขตลาดกระบัง อำเภอลาดกระบัง จังหวัดกรุงเทพมหานคร 10520</span>
-                <span className="flex w-[170px] h-[60px] justify-center items-center gap-5 text-[20px]	text-neutral-50">1</span>
-                <span className="flex w-[170px] h-[60px] justify-center items-center gap-5 text-[20px]	text-neutral-50">2</span>
-                <div className="flex items-center justify-center h-9 w-9 rounded-md border border-[#2A3A50]">
-                  <MoreHorizOutlinedIcon className="text-[#FAFAFA]" />
-                </div>
-              </div>
-            </div>
+                  <td className="px-6 py-4 text-[14px] truncate">
+                    1 ซอยฉลองกรุง 1 เขตลาดกระบัง อำเภอลาดกระบัง จังหวัดกรุงเทพมหานคร 10520
+                  </td>
+                  <td className="text-center text-[20px]">1</td>
+                  <td className="px-6 py-4 text-center text-[20px]">2</td>
+                  <td className="px-6 py-4 flex justify-center">
+                    <div className="h-9 w-9 flex items-center justify-center rounded-md border border-[#2A3A50]">
+                      <MoreHorizOutlinedIcon className="text-[#FAFAFA]" />
+                    </div>
+                  </td>
+                </tr>
 
+                {/* data in Table */}
+                <tr className="justify-center">
+                  <td className="px-6 py-4">
+                    <div className="flex items-center gap-5 truncate">
+                      <img
+                        src="https://www.eng.kmitl.ac.th/wp-content/uploads/2024/06/About-4-B.png"
+                        alt="icon"
+                        className="w-[60px] h-[60px] min-w-[60px] min-h-[60px] flex-shrink-0"
+                      />
+                      <span className="truncate">สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง</span>
+                    </div>
+                  </td>
+
+                  <td className="px-6 py-4 text-[14px] truncate">
+                    1 ซอยฉลองกรุง 1 เขตลาดกระบัง อำเภอลาดกระบัง จังหวัดกรุงเทพมหานคร 10520
+                  </td>
+                  <td className="text-center text-[20px]">1</td>
+                  <td className="px-6 py-4 text-center text-[20px]">2</td>
+                  <td className="px-6 py-4 flex justify-center">
+                    <div className="h-9 w-9 flex items-center justify-center rounded-md border border-[#2A3A50]">
+                      <MoreHorizOutlinedIcon className="text-[#FAFAFA]" />
+                    </div>
+                  </td>
+                </tr>
+
+                {/* data in Table */}
+                <tr className="justify-center">
+                  <td className="px-6 py-4">
+                    <div className="flex items-center gap-5 truncate">
+                      <img
+                        src="https://www.eng.kmitl.ac.th/wp-content/uploads/2024/06/About-4-B.png"
+                        alt="icon"
+                        className="w-[60px] h-[60px] min-w-[60px] min-h-[60px] flex-shrink-0"
+                      />
+                      <span className="truncate">สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง</span>
+                    </div>
+                  </td>
+
+                  <td className="px-6 py-4 text-[14px] truncate">
+                    1 ซอยฉลองกรุง 1 เขตลาดกระบัง อำเภอลาดกระบัง จังหวัดกรุงเทพมหานคร 10520
+                  </td>
+                  <td className="text-center text-[20px]">1</td>
+                  <td className="px-6 py-4 text-center text-[20px]">2</td>
+                  <td className="px-6 py-4 flex justify-center">
+                    <div className="h-9 w-9 flex items-center justify-center rounded-md border border-[#2A3A50]">
+                      <MoreHorizOutlinedIcon className="text-[#FAFAFA]" />
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
-
-
-
         </div>
-      </div>
+      </div >
     </>
   );
 }
 
-{/* <div className="flex items-center px-4"> */ }
