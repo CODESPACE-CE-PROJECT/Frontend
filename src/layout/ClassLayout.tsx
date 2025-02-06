9ac81444af143c568d405544703ae6131e5b92fc
Worathut Jittichai <97940582+WoraPhet@users.noreply.github.com>
2025-02-07 01:30:57 +0700

    update file

--- src/layout/ClassLayout.tsx
+++ src/layout/ClassLayout.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import React, { ReactNode } from "react";
-import ClassRoomNav from "@/components/ClassRoomNav";
-import UserNav from "@/components/UserNav";
+import ClassRoomNav from "@/components/Navbar/ClassRoomNav";
 import { Role } from "@/enum/enum";
 import { usePathname } from "next/navigation";
+import { TopNav } from "@/components/Navbar/TopNav";
 
 interface LayoutProps {
   children: ReactNode;
@@ -26,7 +26,7 @@ const ClassLayout: React.FC<LayoutProps> = ({ children, id }) => {
     <div className="flex flex-row overflow-y-hidden overscroll-none">
       <ClassRoomNav id={id} role={role} />
       <div className="text-[#FAFAFA] my-10 mx-[3.75rem] h-full w-full">
-        <UserNav role={role} />
+        <TopNav />
         {children}
       </div>
     </div>
