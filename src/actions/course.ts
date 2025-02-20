ae8bfc4c49b9f3099105f880a1a8e976b1c08a89
Worathut Jittichai <97940582+WoraPhet@users.noreply.github.com>
2025-02-20 15:10:32 +0700

    fix getAllCourse

--- src/actions/course.ts
+++ src/actions/course.ts
@@ -3,28 +3,34 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
 import { getToken } from "@/lib/session";
 
-export const getAllCourse = async (): Promise<any | null> => {
+export const getAllCourse = async () => {
   const token = await getToken()
-  if (token) {
-      return await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/course`, {
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      }).then((res) => res.data).catch((err) => err);
-  }
+  return await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/course`, {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  }).then((res) => {
+    return {
+      status: res.status,
+      data: res.data.data
+    }
+  }).catch((err: AxiosError) => {
+    return {
+      status: err.status,
+      data: err.response?.data
+    }
+  });
 }
-
 
 export const getpeople = async (courseId: string) => {
   const token = await getToken()
-  if (token) {
-    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-    const response = await axios.get(
-      `${process.env.NEXT_PUBLIC_BACKEND_URL}/course/${courseId}/people`
-    );
-    return response.data.data;
+  return await axios.get(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/course/${courseId}/people`, {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
   }
-  throw new Error("No token available");
+  ).then((res) => res.data.data);
 };
 
 export const getCoursesById = async (courseId: string) => {
@@ -66,8 +72,8 @@ export const editCourse = async (courseId: string, courseData: any) => {
   }
 };
 
-export const uploadCoursePicture = async (courseId:string, picture: File) => {
-  const token = getToken()
+export const uploadCoursePicture = async (courseId: string, picture: File) => {
+  const token = getToken();
   if (token) {
     axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
     try {
@@ -90,34 +96,34 @@ export const uploadCoursePicture = async (courseId:string, picture: File) => {
 };
 
 export const createCourse = async (formData: {
-     title: string;
-     description: string;
-   }) => {
-     try {
-       const token = getToken()
-   
-       if (!token) {
-         alert("คุณไม่ได้รับอนุญาต โปรดเข้าสู่ระบบ");
-         return;
-       }
-   
-       axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-   
-       const response: AxiosResponse = await axios.post(
-         `${process.env.NEXT_PUBLIC_BACKEND_URL}/course`,
-         {
-           title: formData.title,
-           description: formData.description,
-         },
-         {
-           headers: {
-             "Content-Type": "application/json",
-           },
-         }
-       );
-   
-       return response.data;
-     } catch (error) {
-       throw error;
-     }
-   };
+  title: string;
+  description: string;
+}) => {
+  try {
+    const token = getToken();
+
+    if (!token) {
+      alert("คุณไม่ได้รับอนุญาต โปรดเข้าสู่ระบบ");
+      return;
+    }
+
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+    const response: AxiosResponse = await axios.post(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/course`,
+      {
+        title: formData.title,
+        description: formData.description,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
