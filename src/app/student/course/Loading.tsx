09730c1644422a9f78f0fe5eb4df9f6569cc7f1d
Worathut Jittichai <97940582+WoraPhet@users.noreply.github.com>
2025-02-20 14:56:23 +0700

    fix loading

--- src/app/student/course/Loading.tsx
+++
@@ -1,5 +0,0 @@
-import { Loading } from "@/components/Loading/Loading"
-
-export default function LoadingPage () {
-     return <Loading className="w-24 h-24"/>
-}
--- src/app/student/course/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Loading } from "@/components/Loading/Loading";
-
-export default function LoadingPage() {
-  return (
-    <div className="flex flex-col items-center justify-center w-full h-full">
-      <Loading className="size-20" />
-    </div>
-  );
-}
