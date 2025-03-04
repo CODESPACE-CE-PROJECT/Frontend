df6f1de920b8e9b7032417059c5d0e2127609310
unixvextor <withun.wch@gmail.com>
2025-03-04 18:14:04 +0700

    update: enable sse

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,11 +4,10 @@ import { cookies } from "next/headers";
 import { decrypt, deleteSession, getAccessToken } from "@/lib/session";
 import { Role } from "@/enum/enum";
 
-// Define the allowed base paths for each role
 const roleRoutes = {
-  [Role.ADMIN]: "/admin",
-  [Role.TEACHER]: "/teacher",
-  [Role.STUDENT]: "/student",
+  [Role.ADMIN]: "/admin/dashboard",
+  [Role.TEACHER]: "/teacher/course",
+  [Role.STUDENT]: "/student/course",
 };
 
 const publicRoutes = ["/login", "/forgot-password"];
@@ -19,7 +18,6 @@ export default async function middleware(req: NextRequest) {
   
   const refreshToken = (await cookies()).get("refreshToken")?.value;
 
-  // Redirect users to login if they visit "/" without authentication
   if (path === "/" && !refreshToken) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
@@ -43,19 +41,17 @@ export default async function middleware(req: NextRequest) {
   const userRole = payload.role;
   const allowedRoute = roleRoutes[userRole];
 
-  // If user visits "/" and is logged in, redirect them to their role's main page
   if (path === "/") {
-    return NextResponse.redirect(new URL(`${allowedRoute}/course`, req.url));
+    return NextResponse.redirect(new URL(`${allowedRoute}`, req.url));
   }
 
-  // Restrict users from accessing another role's section
   const isAccessingOtherRole =
     (userRole === Role.STUDENT && !path.startsWith("/student")) ||
     (userRole === Role.TEACHER && !path.startsWith("/teacher")) ||
     (userRole === Role.ADMIN && !path.startsWith("/admin"));
 
   if (isAccessingOtherRole && !isPublicRoute) {
-    return NextResponse.redirect(new URL(`${allowedRoute}/course`, req.url));
+    return NextResponse.redirect(new URL(`${allowedRoute}`, req.url));
   }
 
   return NextResponse.next();
