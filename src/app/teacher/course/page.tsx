9f8031e65a5de4ec3da918443a5c4b52a2cf02aa
Worathut Jittichai <97940582+WoraPhet@users.noreply.github.com>
2025-03-25 17:04:45 +0700

    fix couse card

--- src/app/teacher/course/page.tsx
+++ src/app/teacher/course/page.tsx
@@ -11,8 +11,8 @@ import { IProfile } from "@/types/user";
 import { getProfile } from "@/actions/user";
 import { ICourse } from "@/types/course";
 import { TopNav } from "@/components/Navbar/TopNav";
-import { notify, updateNotify } from "@/utils/toast.util"; 
-import { NotifyType } from "@/enum/enum"; 
+import { notify, updateNotify } from "@/utils/toast.util";
+import { NotifyType } from "@/enum/enum";
 
 export default function Page() {
   const [loading, setLoading] = useState<boolean>(true);
@@ -51,51 +51,58 @@ export default function Page() {
   };
 
   const handleSubmit = async () => {
-    
     setErrorMessage("");
-  
-    const id = notify(NotifyType.LOADING, "กำลังสร้างคอร์สเรียน..."); 
-  
+
+    const id = notify(NotifyType.LOADING, "กำลังสร้างคอร์สเรียน...");
+
     try {
       const courseData = new FormData();
       courseData.append("title", formData.title);
       courseData.append("description", formData.description);
       if (imageFile) courseData.append("picture", imageFile);
-  
+
       const newCourse = await createCourse(courseData);
       setCourses((prevCourses) => [...prevCourses, newCourse]);
       setIsModalOpen(false);
-  
+
       if (id) {
-        updateNotify(id, NotifyType.SUCCESS, "สร้างคอร์สเรียนสำเร็จ!"); 
+        updateNotify(id, NotifyType.SUCCESS, "สร้างคอร์สเรียนสำเร็จ!");
+        const { status, data } = await getAllCourse();
+        if (status === 200) setCourses(data);
       }
     } catch (error: any) {
       console.error("Error creating course:", error.response?.data || error);
-  
-  
+
       const errorMessage = error.response?.data?.message || "";
-  
+
       if (errorMessage.includes("Over limit Create Course Per Teacher 3")) {
         setErrorMessage("ไม่สามารถสร้างชั้นเรียนเกิน 3 ชั้นเรียนได้");
         if (id) {
-          updateNotify(id, NotifyType.ERROR, "ไม่สามารถสร้างชั้นเรียนเกิน 3 ชั้นเรียนได้"); 
+          updateNotify(
+            id,
+            NotifyType.ERROR,
+            "ไม่สามารถสร้างชั้นเรียนเกิน 3 ชั้นเรียนได้"
+          );
         }
       } else if (errorMessage.includes("Duplicate course")) {
         setErrorMessage("ไม่สามารถสร้างชั้นเรียนซ้ำได้");
         if (id) {
-          updateNotify(id, NotifyType.ERROR, "ไม่สามารถสร้างชั้นเรียนซ้ำได้"); 
+          updateNotify(id, NotifyType.ERROR, "ไม่สามารถสร้างชั้นเรียนซ้ำได้");
         }
       } else {
         setErrorMessage("เกิดข้อผิดพลาดในการสร้างคอร์สเรียน");
         if (id) {
-          updateNotify(id, NotifyType.ERROR, "เกิดข้อผิดพลาดในการสร้างคอร์สเรียน");
+          updateNotify(
+            id,
+            NotifyType.ERROR,
+            "เกิดข้อผิดพลาดในการสร้างคอร์สเรียน"
+          );
         }
       }
     } finally {
       setLoading(false);
     }
   };
-  
 
   return (
     <>
